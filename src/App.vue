86b6c39a080f5c9671e630393c7ff08b5d94539b
riceawithwheat <115392299+riceawithwheat@users.noreply.github.com>
2023-10-19 08:39:31 +0800

    1019调整

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <global-header :user="currentUser"></global-header>
-
+  <mouse-point></mouse-point>
     <!-- <loader v-if="isLoading" background="rgba(0,0,0,0.8 )" ></loader> -->
     <router-view></router-view>
   <div class="container-fluid">
   <footer class="text-center py-4 text-secondary  mt-6 ">
     <small>
         <ul class="list-inline mb-0">
-          <li class="list-inline-item">© 学罔learnMore</li>
+          <li class="list-inline-item">© learnMore博客</li>
           <li class="list-inline-item">课程</li>
           <li class="list-inline-item">文档</li>
           <li class="list-inline-item">联系</li>
@@ -23,13 +23,15 @@ import { defineComponent, computed, watch } from 'vue'
 import { useStore } from 'vuex'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import GlobalHeader from './components/GlobalHeader.vue'
-import Loader from './components/Loader.vue'
+// import Loader from './components/Loader.vue'
 import { GlobalDataProps } from './store'
 import createMessage from './components/createMessage'
+import mousePoint from './components/mousePoint.vue'
 export default defineComponent({
   name: 'App',
   components: {
-    GlobalHeader
+    GlobalHeader,
+    mousePoint
     // Loader
   },
   setup () {
